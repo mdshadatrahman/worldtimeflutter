29e7adb7fb48f0ffa92a6d5c8360e5ef580e99e0
ShadatRahman <shadat.rahman.464@gmail.com>
2021-05-01 06:19:34 +0600

    added git to project

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+
+class ChooseLocation extends StatefulWidget {
+  @override
+  _ChooseLocationState createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+
+  List<WorldTime> locations = [
+    WorldTime(url: 'Asia/Dhaka', location: 'Dhaka', flag: 'bd.png'),
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+  ];
+
+  void updateTime(index) async{
+    WorldTime instance = locations[index];
+    await instance.getTime();
+    //navigate to home screen
+    Navigator.pop(context, {
+      'location' : instance.location,
+      'flag' : instance.flag,
+      'time' : instance.time,
+      'isDayTime' : instance.isDayTime,
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body:ListView.builder(
+          itemCount: locations.length,
+          itemBuilder: (context, index){
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 4),
+              child: Card(
+                child: ListTile(
+                onTap: (){
+                  updateTime(index);
+                },
+                title: Text(locations[index].location),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage('assets/${locations[index].flag}'),
+                  ),
+              ),
+              ),
+            );
+          }
+      ),
+    );
+  }
+}
